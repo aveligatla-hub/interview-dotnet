df4a91637e1888281617c58fd6d696395bd4e3f0
aveligatla-hub <58155831+aveligatla-hub@users.noreply.github.com>
2019-11-24 21:37:46 -0500

    Add files via upload
    DTOs

---
+++ GroceryStoreAPI/DTO/Item.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GroceryStoreAPI.DTO
+{
+    public class Item
+    {
+        [Required]
+        public Guid ProductId { get; set; }        
+
+        [Required]
+        public int Quantity { get; set; }
+    }
+}
---
+++ GroceryStoreAPI/DTO/Order.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GroceryStoreAPI.DTO
+{
+    public class Order
+    {
+        /// <summary>
+        /// The unique order id
+        /// </summary>
+        [Required]
+        public int Id { get; set; }
+        /// <summary>
+        /// The customer id
+        /// </summary>
+        [Required]
+        public int CustomerId { get; set; }
+
+        /// <summary>
+        /// Items included in an order
+        /// </summary>
+        [Required]
+        public IEnumerable<Item> Items { get; set; }
+
+        /// <summary>
+        /// The date the item(s) are ordered
+        /// </summary>
+        [Required]
+        public DateTime Date { get; set; }
+    }
+}

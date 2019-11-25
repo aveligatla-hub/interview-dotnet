fb9d1470f365b261bf7f1f6e8800b0b3738b7523
aveligatla-hub <58155831+aveligatla-hub@users.noreply.github.com>
2019-11-24 21:30:15 -0500

    Add files via upload

---
+++ DTO/Customer.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GroceryStoreAPI.DTO
+{
+    public class Customer
+    {
+        /// <summary>
+        /// The customer's unique id
+        /// </summary>
+        [Required]
+        public int Id { get; set; }
+        /// <summary>
+        /// The customer's Name
+        /// </summary>
+        [Required]
+        public string Name { get; set; }        
+    }
+}
